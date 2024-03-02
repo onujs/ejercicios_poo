37c4837823872e709ce708fdafaefa07beab328d
vialcrim <60139890+onujs@users.noreply.github.com>
2024-03-02 12:31:28 -0500

    concepto de objetos

---
+++ poo.js
@@ -0,0 +1,64 @@
+//Crear objetos
+
+/*
+let vehiculo = {
+    
+    //Propiedad
+    color: 'rojo',
+    marca: 'chevrolet',
+    modelo: 2020,
+
+    //Metodos
+    pitar () {
+        return 'Pi pi piiiiii!!!!!';
+    },
+    acelerar() {
+        return 'Estoy acelerando';
+    }
+}
+
+console.log(vehiculo.pitar);
+console.log(vehiculo.acelerar);
+
+
+let cursos = ["Sistemas", "Ingles", "Contaduria"]
+
+let estudiante = {
+    nombre: "Stiven",
+    edad: 19,
+    sexo: 'Masculino',
+    cursos: ["Sistemas", "Electronica", "administracion de empresas"],
+}
+
+//Crear un objeto llamado estudiante donde sus propiedades sean
+// Nombre, Edad, Sexo y Cursos. Los cursos pueden ser varios
+console.log(estudiante,cursos);
+*/
+
+
+
+let almacen = [
+    {
+        marca: 'iPhone',
+        precio: 1500000,
+        garantia: true,
+        colores: {
+            opcion1: 'Metalizado',
+            opcion2: 'Dorado',
+            opcion2: 'Negro'
+        },
+        almacenamiento: '500 GB',
+        camaraFrontal: true,
+        resolucionCamaraFrontal: '100 Mps',
+    },
+    {
+        marca: 'Samsung',
+        precio: 2000000,
+        garantia: false,
+        colores: {
+            opcion1: 'Rosado',
+            opcion2: 'Azul',
+            opcion2: 'Blanco'
+        }
+    }
+];
