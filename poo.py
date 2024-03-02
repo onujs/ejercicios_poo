2c69957f11104aba619e262ed51928b2f2128585
vialcrim <60139890+onujs@users.noreply.github.com>
2024-03-02 13:45:37 -0500

    ejercicio de phyton

---
+++ poo.py
@@ -0,0 +1,24 @@
+class Carro:
+    def __init__(self, marca, modelo, color):
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+        self.encendido = False
+
+    def encender(self):
+        if not self.encendido:
+            print("El carro esta encendido")
+            self.encendido = True
+        else:
+            print("El carro ya esta encendido")
+
+    def apagar (self):
+        if self.encendido:
+            print("El carro esta apagado")
+        else:
+            print("El carro ya esta apagado")
+
+#Crear una instancia e la Carro
+carrodeJulian = Carro("Camaro", 2022, "Negro")
+
+print(carrodeJulian.marca)
